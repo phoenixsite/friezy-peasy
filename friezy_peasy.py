3731d77fcf44d2fbf564688eac81d36a83ac467c
Carlos Romero <phoenixsitegit@outlook.com>
2024-02-21 14:01:50 +0100

    Pythonic function names, docs and more mem eff

--- friezy_peasy.py
+++ friezy_peasy.py
@@ -10,31 +10,29 @@ import numpy as np
 import argparse
 
 MAX_LINE = 60
+PATTERNS = ['p1', 'p2', 'p1m1', 'p2mm', 'p11m', 'p11g', 'pcm', 'p2mg']
 
-def saveImage(im, output_path):
-  """
-  Save the generated image to a file.
-
-  :param im: Array of concatenated unit cells
-  :type im: numpy.ndarray
-  :param output_path: Path route where the image is saved.
-  :type: str
-  """
-  
-  im = np.hstack(im)
-  cv.imwrite(output_path, im)
-
-def buildFrieze(im, pattern, nreps):
+def build_frieze(im, pattern, nreps):
   """
   Generate the frieze image given an UIC frieze pattern and the number
   of repetitions of the image.
 
   :param im: Image 
   :type im: cv.Mat
-  :param pattern: UIC frieze pattern
+  :param pattern: Frieze pattern
   :type pattern: str
-  :type nreps: Number of times the unit cell is repeated along the
-  frieze.
+  :param nreps: Number of times the unit cell is repeated along the
+  frieze. The possible values are:
+
+          1. ``'p1'``
+          2. ``'p2'``
+          3. ``'p1m1'``
+          4. ``'p2mm'``
+          5. ``'p11m'``
+          6. ``'p11g'``
+          7. ``'pcm'`` or ``'p2mg'``
+
+  :type nreps: int
   :rparam Array of concatenated unit cells that represents the frieze.
   :rtype list
   """
@@ -43,58 +41,39 @@ def buildFrieze(im, pattern, nreps):
     unit_cell = [im]
     
   elif(pattern == 'p2'):
-    
-    im2 = cv.rotate(im, cv.ROTATE_180)
-    unit_cell = [im, im2]
+    unit_cell = [im, cv.rotate(im, cv.ROTATE_180)]
     
   elif(pattern == 'p1m1'):
     
     # 1 = vertical line reflection
     # 0 = horizontal line reflection
-    im2 = cv.flip(im, 1)
-    unit_cell = [im, im2]
+    unit_cell = [im, cv.flip(im, 1)]
     
   elif(pattern == 'p2mm'):
     
-    im2 = cv.flip(im, 0)
-
-    # Snap vertically two images
-    im3 = np.concatenate((im, im2), axis=0)
-    im4 = np.flip(im3, 1)
-    unit_cell = [im3, im4]
+    unit_cell = [np.concatenate((im, cv.flip(im, 0)), axis=0)]
+    unit_cell.append(np.flip(unit_cell[0], 1))
 
   elif(pattern == 'p11m'):
-    
-    im2 = cv.flip(im, 0)
-    im3 = np.concatenate((im, im2), axis=0)
-    unit_cell = [im3]
+    unit_cell = [np.concatenate((im, cv.flip(im, 0)), axis=0)]
 
   elif(pattern == 'p11g'):
     
     im2 = cv.flip(im, 0)
     firsthalf = im2[:, 0:int(im2.shape[1] / 2)]
-    secondhalf = im2[: ,int(im2.shape[1] / 2):int(im2.shape[1])]
+    secondhalf = im2[: , int(im2.shape[1] / 2):int(im2.shape[1])]
 
     # Snap horizontally two images
-    im3 = np.concatenate((secondhalf,firsthalf), axis=1)
-    im4 = np.concatenate((im,im3), axis=0)
+    im3 = np.concatenate((secondhalf, firsthalf), axis=1)
+    im4 = np.concatenate((im, im3), axis=0)
     unit_cell = [im4]
+
+  elif(pattern in ('pcm', 'p2mg')):
     
-  elif(pattern == 'pcm'):
-    
-    im2 = cv.flip(im, 1)
-    im3 = np.concatenate((im, im2), axis=1)
-    im4 = cv.rotate(im3, cv.ROTATE_180)
-    unit_cell = [im3, im4]
+    unit_cell = [np.concatenate((im, cv.flip(im, 1)), axis=1)]
+    unit_cell.append(cv.rotate(unit_cell[0], cv.ROTATE_180))
 
-  else:
-    raise RuntimeException("The given pattern is not valid")
-
-  out = []
-  for i in range(nreps):
-    out.extend(unit_cell)
-
-  return out
+  return np.hstack(unit_cell * nreps)
 
 
 if __name__ == '__main__':
@@ -103,23 +82,21 @@ if __name__ == '__main__':
     prog='friezy_peasy',
     description='Frieze images generator')
 
-  pattern_choices = ['p1', 'p2', 'p1m1', 'p2mm', 'p11m', 'p11g', 'pcm']
-
   parser.add_argument(
     'im_path',
     metavar='image-path',
-    help='Image path which is used to generate the frieze.')
+    help='Path to the image used to generate the frieze.')
 
   parser.add_argument(
     'output_path',
     metavar='destiny-path',
-    help='Destiny image path where the generated image will be saved.')
+    help='Path to the file where the generated image will be saved.')
 
   parser.add_argument(
     'pattern',
-    choices=pattern_choices,
+    choices=PATTERNS,
     metavar='pattern',
-    help='Frieze pattern group that will be used to generate the frieze.')
+    help='IUCr frieze pattern group that will be used to generate the frieze.')
 
   parser.add_argument(
     'nreps',
@@ -130,5 +107,5 @@ if __name__ == '__main__':
 
   args = parser.parse_args()
   im = cv.imread(args.im_path)
-  output_im = buildFrieze(im, args.pattern, args.nreps)
-  saveImage(output_im, args.output_path)
+  output_im = build_frieze(im, args.pattern, args.nreps)
+  cv.imwrite(args.output_path, output_im)
